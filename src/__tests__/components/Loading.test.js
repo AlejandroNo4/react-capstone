206d16a14a114d6461951cf9203057e3d224ce7d
Alejandro Contreras <kdta15@outlook.com>
2021-08-30 09:10:01 -0500

    Run and fix ESlint linters.

--- src/__tests__/components/Loading.test.js
+++ src/__tests__/components/Loading.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
-import Loading from '../../containers/Loading';
+import Loading from '../../components/Loading';
 
 describe('Loading component', () => {
   it('Render component wothout errors', () => {
